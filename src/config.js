e406e40a623424eb23a4917c7e7a8ff6eaa3e3dd
SultanaK <kr.shampa@yahoo.com>
2020-09-12 14:26:19 -0500

    deploy ready

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  //DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/noteful',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://housewill@localhost/housewill',
-  //TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin@localhost/blogful-test'
    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://housewill@localhost/housewill-test'
 
 }
